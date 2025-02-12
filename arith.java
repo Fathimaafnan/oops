85e226ae0132fc892ae16c87302858141807545a
Fathimaafnan <afnanrasak@gmail.com>
2025-02-12 11:38:14 +0530

    Add files via upload

---
+++ arith.java
@@ -0,0 +1,17 @@
+public class arith{
+public static void main(String[] args){
+int a;
+int b;
+int c;
+int d;
+a=1+3;
+System.out.println(a);
+b=5-7;
+System.out.println(b);
+c=8*6;
+System.out.println(c);
+d=100/4;
+System.out.println(d);
+}
+}
+
